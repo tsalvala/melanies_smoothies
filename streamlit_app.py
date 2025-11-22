a0c9ade65ff77b8975a9a34ae259996c1b537d22
tsalvala <tommaso.salvalaggio@sdggroup.com>
2025-11-22 18:19:02 +0100

    Update streamlit_app.py
    corrected typo in connection setting

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ st.write("Choose the fruits you want in your custom Smoothie!")
 name_on_order = st.text_input('Name on Smoothie')
 st.write('The name on your Smoothie will be:', name_on_order)
 
-csx = st.connexion('snowflake')
+cnx = st.connection('snowflake')
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
