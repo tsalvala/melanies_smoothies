29e5f50cc32b5a5dd118b5da0699e9cd1051d0b5
tsalvala <tommaso.salvalaggio@sdggroup.com>
2025-11-24 09:14:26 +0100

    Update streamlit_app.py
    put response in a dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,6 +36,7 @@ if ingredients_list:
 # Quando si importa nuova libreria bisogna aggiornare requirements.txt e fare reboot
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
+#st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 
